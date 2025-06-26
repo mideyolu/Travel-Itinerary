c04680aa58ce7af8db1c77ec60d924fab9f86ca7
mideyolu <olumideoluwuyi91@gmail.com>
2025-06-26 18:49:16 +0100

    Included Itinerary Agent config

--- app/agents/itinerary_agent.py
+++ app/agents/itinerary_agent.py
@@ -2,13 +2,14 @@
 
 import json
 from agno.agent import Agent
-from app.utils.helpers import load_llm
+from app.utils.helpers import load_llm, load_serpapi_tools
 from app.core.exceptions import ItineraryPlannerAgentError, MissingParameterError
 from app.utils.logging import logging
 
-# Hotel Search Agent
+# Itinerary Planner Agent
 itinerary_agent = Agent(
     model=load_llm(),
+    tools=[load_serpapi_tools().search_google],
     role="Plan detailed daily itineraries with activities, restaurants, and logistics for travel destinations.",
     show_tool_calls=True,
     markdown=False,
@@ -71,7 +72,7 @@ def generate_itinerary(prompt: str):
 
         {prompt}
         """
-        
+
         result = itinerary_agent.run(
             message=full_prompt,
         )
